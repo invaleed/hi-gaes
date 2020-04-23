52adf0c05e1007ef38d26072170de0ffc12a3c12
invaleed <invaleed@gmail.com>
2020-04-24 05:54:30 +0700

    Create gaes.py

---
+++ gaes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print "Content-type: text/html\n\n"
+print "<html>\n<body>"
+print "<div style=\"width: 100%; font-size: 20px; font-weight: bold; text-align: center;\">"
+print "Hi Gaes"
+print "</div>\n</body>\n</html>"
